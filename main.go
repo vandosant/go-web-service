e7e9c7f313009b3a4ed1109a7cb9c91fc0493e50
vandosant <sjskender@gmail.com>
2015-04-27 11:20:30 -0600

    Do not export GenerateMarkdown func

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.HandleFunc("/markdown", generateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.ListenAndServe(":"+port, nil)
 }
 
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+func generateMarkdown(rw http.ResponseWriter, r *http.Request) {
 	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
 	rw.Write(markdown)
 }
